c3d7c834a4380b068a40115f42d04c52ef564ef3
starsolutionads-droid <starsolutionads@gmail.com>
2026-04-10 14:22:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-function toggleService(el){
+function toggle(el){
 
-  document.querySelectorAll('.details').forEach(d=>{
-    if(d !== el.nextElementSibling){
-      d.style.display = "none";
+  let ul = el.nextElementSibling;
+
+  document.querySelectorAll('.service ul').forEach(list=>{
+    if(list !== ul){
+      list.style.display = "none";
     }
   });
 
-  let box = el.nextElementSibling;
-
-  box.style.display = box.style.display === "block" ? "none" : "block";
+  ul.style.display = ul.style.display === "block" ? "none" : "block";
 }
