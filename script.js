52b37ed6284facc8c36d2205d71507e19d49e37b
starsolutionads-droid <starsolutionads@gmail.com>
2026-04-10 13:19:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
-console.log("Website Loaded");
+function showSection(id) {
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth"
-    });
+  document.querySelectorAll('.section').forEach(sec => {
+    sec.classList.remove('active');
+    sec.classList.add('hidden');
   });
-});
+
+  document.getElementById(id).classList.add('active');
+}
 
 function toggleService(element) {
-
-  document.querySelectorAll('.service-details').forEach(item => {
-    if(item !== element.nextElementSibling){
-      item.style.display = "none";
-    }
-  });
-
   let details = element.nextElementSibling;
 
-  if (details.style.display === "block") {
+  if(details.style.display === "block"){
     details.style.display = "none";
   } else {
     details.style.display = "block";
